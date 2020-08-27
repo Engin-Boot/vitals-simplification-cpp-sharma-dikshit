3a120406a49f906a98c0511ff5631fb2696fa50d
Dikshit <sharma.dikshit001@gmail.com>
2020-08-27 16:12:40 +0530

    Refactored the code

--- checker.cpp
+++ checker.cpp
@@ -1,26 +1,20 @@
 #include <assert.h>
+#include <climits>
 
-const int spo2 = 90;
-const int bpmLimits[] = {70, 150};
-const int respRateLimits[] = {30, 95};
-
-bool bpmCheck(float bpm) {
-  return (bpm >= bpmLimits[0] && bpm < bpmLimits[1]);
-}
-
-bool spo2Check(float spo2) {
-  return spo2 >= 90;
-}
-
-bool respRateCheck(float respRate) {
-  return (respRate >= respRateLimits[0] && respRate < respRateLimits[1]);
+bool vitalIsOkay(float value, int lower_bound, int upper_bound) {
+  return value >= lower_bound && value < upper_bound;
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return bpmCheck(bpm) && spo2Check(spo2) && respRateCheck(respRate);
+  return vitalIsOkay(bpm, 70, 150) && 
+          vitalIsOkay(spo2, 90, INT_MAX) &&
+          vitalIsOkay(respRate, 30, 95);
 }
 
 int main() {
+  assert(vitalIsOkay(96, 70, 150) == true);
+  assert(vitalIsOkay(50, 30, 95) == true);
+  assert(vitalIsOkay(152, 70, 150) == false);
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
 }
