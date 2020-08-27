85f4e214d8d3721ff371c26fa555b64d44c6b95c
Dikshit <sharma.dikshit001@gmail.com>
2020-08-27 16:44:20 +0530

    Refactored the code

--- checker.cpp
+++ checker.cpp
@@ -1,14 +1,18 @@
 #include <assert.h>
 #include <climits>
 
+const int bpmLimits[] = {70, 150};
+const int spo2Limits[] = {90, INT_MAX}; //has only lower bound so treated the the upper bound as INT_MAX
+const int respRateLimits[] = {30, 95};
+
 bool vitalIsOkay(float value, int lower_bound, int upper_bound) {
   return value >= lower_bound && value < upper_bound;
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return vitalIsOkay(bpm, 70, 150) && 
-          vitalIsOkay(spo2, 90, INT_MAX) &&
-          vitalIsOkay(respRate, 30, 95);
+  return vitalIsOkay(bpm, bpmLimits[0], bpmLimits[1]) && 
+          vitalIsOkay(spo2, spo2Limits[0], spo2Limits[1]) &&
+          vitalIsOkay(respRate, respRateLimits[0], respRateLimits[1]);
 }
 
 int main() {
